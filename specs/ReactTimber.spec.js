271754b389f89e883baa4c08900457fc203019b3
Clay Diffrient <cdiffrient@instructure.com>
2015-01-20 20:37:31 -0700

    Adds many pending tests and another functioning one

--- specs/ReactTimber.spec.js
+++ specs/ReactTimber.spec.js
@@ -33,8 +33,28 @@ describe('ReactTimber', function() {
   it('should fire the onClick event when pressing enter', function () {
     Simulate.keyUp(timber.refs['node-0'].getDOMNode(), {key: 'Enter'});
     ok(timberApp.refs.infoPane.getDOMNode().textContent === 'Enter');
-  })
+  });
 
+  xit('should open a collapsed node when the right arrow is pressed', function () {
+    timber.setState({
+      focusElement: timber.refs['node-1']
+    }, function () {
+      Simulate.keyUp(timber.refs['node-1'].getDOMNode(), {key: 'ArrowRight'});
+      // This should have worked, but didn't :(
+      // ok(!timber.refs['node-1'].state.collapsed);
+    });
+  });
+
+  it('should move to the first child of an expanded node when the right arrow is pressed')
+  it('should do nothing when the right arrow is pressed on an end node.', function () {
+    Simulate.keyUp(timber.refs['node-0'].getDOMNode(), {key: 'ArrowRight'});
+    ok(timber.refs['node-0'].state.collapsed);
+  });
+
+  it('should close an expanded node when the left arrow is pressed');
+  it('should move focus to the parent node when left arrow is pressed on a collapsed node');
+
+  it('should remember the last visited node when navigating away then back');
   it('should move focus up when the up arrow is pressed');
   it('should move focus down when the down arrow is pressed');
 
